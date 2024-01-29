102f4a3f971e4fe1425f21ef3b316ff899f50ed2
Andrei.Petr <andreipetrovic@yandex.ru>
2024-01-29 11:26:58 -0800

    create new repoz where configuration is java code

---
+++ src/main/java/ru/zezyulin/springcource/config/MySpringMvcDispatcherServlet.java
@@ -0,0 +1,20 @@
+package ru.zezyulin.springcource.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServlet extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
